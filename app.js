2f812a4af212a415f232845fb26685ca2f736ea6
liviaalcantaraf <liviaalcantaraf@gmail.com>
2022-03-04 23:46:05 +0000

    corrected modal inicial js

--- app.js
+++ app.js
@@ -7,20 +7,16 @@ menu.addEventListener("click", function () {
 });
 
 //Modal access
-const modal = document.getElementId("#email-modal");
+const modal = document.getElementById("email-modal");
 const openBtn = document.querySelector(".main-btn");
 const closeBtn = document.querySelector(".close-btn");
-
-//ClickEvents
 
 openBtn.addEventListener("click", () => {
   modal.style.display = "block";
 });
-
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
